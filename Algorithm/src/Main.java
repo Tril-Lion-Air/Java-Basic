1001affad175f61b5be9050c09442834cfb92a54
Trilly <tttrillyyy@gmail.com>
2023-02-03 19:01:16 +0900

    ps

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -2,35 +2,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+	public static int bfs(int start, int end, int cnt) {
+		
+		int now = start;
+		boolean[] visited = new boolean[Math.max(start, end)*2];
+		
+		Queue<Integer> que = new LinkedList<>();
+		que.offer(now);
+		
+		while(!que.isEmpty()) {
+			now = que.poll();
+			int[] nexts = {now+1, now-1, now*2};
+			if (now == end) return cnt;
+			if (!visited[now]) {
+				visited[now] = true;
+				cnt++;
+				for (int next : nexts) {
+					if (!visited[next]) {
+						que.offer(next);
+					}
+				}
+			}
+		}
+		
+		return cnt;
+	}
 
 	public static void main(String[] args) throws IOException {
 		
-		List<Integer> iList1 = new ArrayList<>();
-		for(int i=1;i<=5;i++) {
-			iList1.add(i);
-
-		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 		
-		int[] iArr = {1, 2, 3, 4, 5};
-//		List<Integer> iList2 = new ArrayList<>(Arrays.asList());
-//		list2.add(6);
-		List list = Arrays.asList(iArr);
-		System.out.println(list.get(0));
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 		
-
-		
-		List<String> sList1 = new ArrayList<String>(Arrays.asList("1","2","3","4","5"));
-		List<String> sList2 = Arrays.asList("11","2222","33","44","55");
-		
-		List<Integer> sList3 = new ArrayList<>(Arrays.asList(1,2,3,4,5));
-
-		
-		System.out.println(iList1.toString());
-//		System.out.println(iLst2.toString());
-
-		System.out.println(sList1);
-		System.out.println(sList2);
-		System.out.println(sList3.get(0) + sList3.get(1));
-		
+		System.out.println(bfs(n,k,0));
+	
 	}
 }
